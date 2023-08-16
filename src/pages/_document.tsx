47820d778706199dd65dac66541880a519b35d47
hoangbao0201 <hoangbao020103@gmail.com>
2023-08-17 00:03:34 +0700

    Add Google Search

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <meta
+                    name="google-site-verification"
+                    content="oixwdquX6MnMAWSPI4eKn9BlO7ooi292xlAwt-vs9_o"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
