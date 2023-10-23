2351996e7a1ce43949a17b234139ca0dda01dd07
hoangbao0201 <hoangbao020103@gmail.com>
2023-10-24 00:21:00 +0700

    Add google adss

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,19 @@ export default function Document() {
                     name="google-site-verification"
                     content="oixwdquX6MnMAWSPI4eKn9BlO7ooi292xlAwt-vs9_o"
                 />
+
+                {/* Add ads */}
+                <link rel="text" href="/ads.txt" />
+                <meta
+                    name="google-adsense-account"
+                    content="ca-pub-6688547661590907"
+                />
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6688547661590907"
+                    crossOrigin="anonymous"
+                ></script>
+
             </Head>
             <body>
                 <Main />
