e5c889b2c57df7eea449e82e4bd4010645c05018
hoangbao0201 <hoangbao020103@gmail.com>
2024-10-14 11:46:01 +0700

    Update UI

---
+++ src/components/modules/Admin/AdminBlogTemplate/Form/index.tsx
@@ -0,0 +1,150 @@
+import dynamic from "next/dynamic";
+import { ChangeEvent, useRef, useState } from "react";
+
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+import Editor from "react-markdown-editor-lite";
+import "react-markdown-editor-lite/lib/index.css";
+
+import { NextPageWithLayout } from "@/pages/_app";
+import AdminLayout from "@/components/layouts/AdminLayout";
+import { convertTextToSlug } from "@/utils/convertTextToSlug";
+import { ShowToastify } from "@/components/share/ShowToastify";
+import LoadingDots from "@/components/share/Loading/LoadingDots";
+
+const AdminFormBlogTemplate : NextPageWithLayout = () => {
+
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const [dataBasicBlog, setDataBasicBlog] = useState({
+        title: "",
+        description: "",
+        thumbnail: "",
+        blogHashtags: "",
+    })
+    const [contentBlog, setContentBlog] = useState<string>("");
+    const [isLoad, setIsLoad] = useState(false);
+
+
+    const onChangeContentBlog = (
+        e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+    ) => {
+        setDataBasicBlog({
+            ...dataBasicBlog,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleCreateBlog = async () => {
+        const { title, blogHashtags } = dataBasicBlog;
+        if(!title || !contentBlog || !blogHashtags) {
+            ShowToastify({
+                data: "Chưa điền đủ thông tin",
+                type: "error"
+            })
+            console.log(title + "-" + contentBlog + "-" + blogHashtags)
+            return;
+        }
+        setIsLoad(true);
+
+        try {
+            const blogRes = await axios.post("/api/blog/create", {
+                userId: "64d39a038ecc99b24ffe78f1",
+                ...dataBasicBlog,
+                content: contentBlog,
+                slug: convertTextToSlug(dataBasicBlog.title) || "",
+                blogHashtags: dataBasicBlog.blogHashtags.split("||"),
+            })
+
+            if(blogRes.data.success) {
+                setIsLoad(false);
+                ShowToastify({
+                    data: "Tạo bài viết thành công",
+                    type: "success",
+                });
+            }
+        } catch (error) {
+            setIsLoad(false);
+        }
+    }
+
+    const handleEditorChange = ({ html, text } : { html: string, text: string }) => {
+        setContentBlog(text);
+    };
+
+    return (
+        <div>
+            <div className="bg-white py-5 px-5 mb-4 rounded-xl border">
+                <div>
+                    <label className="block mb-2 font-semibold">Tiêu đề</label>
+                    <input
+                        placeholder=""
+                        name="title"
+                        value={dataBasicBlog.title}
+                        onChange={onChangeContentBlog}
+                        className="input-info mb-4"
+                    />
+                </div>
+                <div>
+                    <label className="block mb-2 font-semibold">Đường dẫn</label>
+                    <input
+                        placeholder=""
+                        disabled={true}
+                        value={convertTextToSlug(dataBasicBlog.title) || ""}
+                        className="input-info mb-4"
+                    />
+                </div>
+                <div>
+                    <label className="block mb-2 font-semibold">Tag</label>
+                    <input
+                        placeholder=""
+                        name="blogHashtags"
+                        value={dataBasicBlog.blogHashtags}
+                        onChange={onChangeContentBlog}
+                        className="input-info mb-4"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block mb-2 font-semibold">Mô tả</label>
+                    <textarea
+                        placeholder=""
+                        name="description"
+                        value={dataBasicBlog?.description}
+                        onChange={onChangeContentBlog}
+                        className="input-info min-h-[200px]"
+                    />
+                </div>
+            </div>
+            <div className="bg-white py-5 px-5 mb-4 rounded-xl border">
+                <div className="">
+                    
+                    <Editor
+                        value={contentBlog}
+                        className="min-h-screen w-full"
+                        onChange={handleEditorChange}
+                        renderHTML={(text) => {
+                            return (
+                                <ReactMarkdown
+                                    sourcePos={true}
+                                    // children={text}
+                                    className="prose"
+                                >
+                                    {text}
+                                </ReactMarkdown>
+                            )
+                        }}
+                    />
+
+                </div>
+            </div>
+
+            <div className="bg-white py-4 px-5 mb-4 rounded-xl border">
+                <div className="flex justify-end">
+                    <button disabled={isLoad} onClick={handleCreateBlog} className="disabled:opacity-90 bg-blue-600 enabled:hover:bg-blue-700 transition-all border px-3 py-2 rounded-md text-white font-semibold">
+                        Tạo blog {isLoad && <LoadingDots color="#ffff" />}
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default AdminFormBlogTemplate;
--- src/components/share/ButtonContact.tsx
+++ src/components/share/ButtonContact.tsx
@@ -4,10 +4,10 @@ import { styled } from "styled-components";
 import Link from "next/link";
 import siteMetadata from "@/siteMetadata";
 
-const ButtonContactStyles = styled.div`
+const ButtonContactStyles = styled.div<{ $primary?: string }>`
     z-index: 11;
-    width: 80px;
-    height: 80px;
+    width: 70px;
+    height: 70px;
     cursor: pointer;
     visibility: visible;
     background-color: transparent;
@@ -15,16 +15,17 @@ const ButtonContactStyles = styled.div`
     left: 0;
     bottom: 0;
     display: block;
+    position: relative;
 
     & > span {
-        width: 64px;
-        height: 64px;
-        top: 8px;
-        left: 8px;
+        width: 60px;
+        height: 60px;
+        top: 4px;
+        left: 4px;
         position: absolute;
         opacity: 0.7;
-        box-shadow: 0 0 0 0 #35bee8;
-        background-color: #35bee8;
+        box-shadow: 0 0 0 0 ${props => props?.$primary ? props?.$primary : "#35bee8"};
+        background-color: ${props => props?.$primary ? props?.$primary : "#35bee8"};
         border-radius: 50%;
         border: 2px solid transparent;
         transition: all 0.5s;
@@ -33,6 +34,8 @@ const ButtonContactStyles = styled.div`
     }
 
     & > div {
+        top: 12px;
+        left: 12px;
         animation: phone-vr-circle-fill 4s infinite ease-in-out;
     }
 `;
@@ -45,24 +48,73 @@ const ButtonContact = ({ children }: ButtonContactProps) => {
     return (
         <>
             <div className="fixed left-3 bottom-3 flex flex-col items-center">
-
-                <ButtonContactStyles>
-                    <span>
-                    </span>
-                    <div className="flex items-center justify-center bg-blue-500 top-[18px] left-[18px] w-11 h-11 rounded-full overflow-hidden absolute leading-10">
-                        <Link href={`https://zalo.me/${siteMetadata?.phone || "0971183153"}`} target="_blank">
-                            <Image
-                                // unoptimized
-                                width={30}
-                                height={30}
-                                alt="Image zalo"
-                                src={`/static/images/icon-zalo.png`}
-                                className="w-6 h-6 object-cover flex-shrink-0"
-                            />
-                        </Link>
-                    </div>
-                </ButtonContactStyles>
-                <span className="text-white font-semibold bg-blue-500 px-2 rounded-md">Liên hệ</span>
+                <div className="top-[18px] left-[18px]">
+                    <ButtonContactStyles $primary="#60a5fa">
+                        <span></span>
+                        <div className="flex items-center justify-center bg-blue-400 w-11 h-11 rounded-full overflow-hidden absolute leading-10">
+                            <Link
+                                href={`https://zalo.me/${
+                                    siteMetadata?.phone || "0971183153"
+                                }`}
+                                target="_blank"
+                            >
+                                <Image
+                                    // unoptimized
+                                    width={30}
+                                    height={30}
+                                    alt="Image zalo"
+                                    src={`/static/images/icon-zalo.png`}
+                                    className="w-6 h-6 object-cover flex-shrink-0"
+                                />
+                            </Link>
+                        </div>
+                    </ButtonContactStyles>
+                    <ButtonContactStyles $primary="#2563eb">
+                        <span></span>
+                        <div className="flex items-center justify-center bg-blue-600 w-11 h-11 rounded-full overflow-hidden absolute leading-10">
+                            <Link
+                                href={`https://zalo.me/${
+                                    siteMetadata?.phone || "0971183153"
+                                }`}
+                                target="_blank"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 320 512"
+                                    width={30}
+                                    height={30}
+                                    className="p-1 fill-white"
+                                >
+                                    <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                                </svg>
+                            </Link>
+                        </div>
+                    </ButtonContactStyles>
+                    <ButtonContactStyles $primary="#22c55e">
+                        <span></span>
+                        <div className="flex items-center justify-center bg-green-500 w-11 h-11 rounded-full overflow-hidden absolute leading-10">
+                            <Link
+                                href={`https://zalo.me/${
+                                    siteMetadata?.phone || "0971183153"
+                                }`}
+                                target="_blank"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 512 512"
+                                    width={30}
+                                    height={30}
+                                    className="p-1 fill-white"
+                                >
+                                    <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                                </svg>
+                            </Link>
+                        </div>
+                    </ButtonContactStyles>
+                </div>
+                <span className="text-white font-semibold bg-blue-500 px-2 rounded-md">
+                    Liên hệ
+                </span>
             </div>
         </>
     );
